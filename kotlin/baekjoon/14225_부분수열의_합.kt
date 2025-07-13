d6e041d8be576bbec0d1829af309a9a61b11dcb7
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-07-13 21:08:49 +0900

    solve: [BOJ 14225] 부분수열의 합 (Silver 1)

---
+++ kotlin/baekjoon/14225_부분수열의_합.kt
@@ -0,0 +1,22 @@
+package baekjoon
+
+fun main() {
+    val n = readln().toInt()
+    val arr = readln().split(" ").map { it.toInt() }
+    val check = BooleanArray(2_000_001) { false }
+
+    for (i in 1 until (1 shl n)) {
+        var s = 0
+        for (j in 0 until n) {
+            if (i and (1 shl j) != 0) s += arr[j]
+        }
+        check[s] = true
+    }
+
+    for (i in 1 until check.size) {
+        if (!check[i]) {
+            println(i)
+            return
+        }
+    }
+}
