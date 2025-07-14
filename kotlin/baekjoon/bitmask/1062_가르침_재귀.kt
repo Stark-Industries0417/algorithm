21a83ff7b48cece1594878c05f8f5d60cd9a3959
kwon <anstjdrnjs3@naver.com>
2025-07-15 07:58:25 +0900

    pending: 1062 가르침 - 재귀 방식 구현중

---
+++ kotlin/baekjoon/bitmask/1062_가르침_재귀.kt
@@ -0,0 +1,42 @@
+package bitmask
+
+import kotlin.math.max
+
+fun main() {
+    val (n, k) = readln().split(" ").map { it.toInt() }
+
+    if (k < 5) {
+        println(0)
+        return
+    }
+
+    if (k >= 26) {
+        println(n)
+        return
+    }
+
+    val words = IntArray(n) {
+        readln().fold(0) { mask, char ->
+            mask or (1 shl (char - 'a'))
+        }
+    }
+
+    var answer = 0
+    fun go(mask: Int, idx: Int, count: Int) {
+        if (count == k) {
+            val num = words.count { word ->
+                word and mask == word
+            }
+            answer = max(answer, num)
+            return
+        }
+
+        go(mask or (1 shl idx), idx + 1, count + 1)
+        go(mask, idx + 1, count)
+    }
+    val defaultMask = "antic".fold(0) { mask, char ->
+        mask or (1 shl (char - 'a'))
+    }
+    go(defaultMask, 0, 5)
+    println(answer)
+}
