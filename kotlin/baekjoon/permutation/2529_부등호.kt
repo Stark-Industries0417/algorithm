91cb8d63d78311ee0b50ffe04c97b035d8afdaf7
kwon <anstjdrnjs3@naver.com>
2025-07-24 08:46:06 +0900

    pending: 2529 부등호 비트마스크 시도

--- kotlin/baekjoon/permutation/2529_부등호.kt
+++ kotlin/baekjoon/permutation/2529_부등호.kt
@@ -7,19 +7,19 @@ fun main() {
     for (i in 1 until (1 shl 9)) {
         var idx = 0
         var ans = ""
-        for (j in 1 until 10) {
+        for (j in 0 until 10) {
             if (i and (1 shl j) != 0) {
-                ans += j.toString()
                 val result = operate(idx, op[idx], ans)
                 if (!result) break
                 idx++
+                ans += j.toChar()
             }
         }
     }
 }
 
 fun operate(idx: Int, op: String, ans: String): Boolean {
-    if (idx == 0) return false
+    if (idx == 0) return true
     if (op == "<")
         if (ans[idx - 1].digitToInt() > ans[idx].digitToInt()) return false
     if (op == ">")
