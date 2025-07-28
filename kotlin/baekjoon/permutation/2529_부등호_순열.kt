76736b80ef4c59a01898c5ff4b77f2296dbe0561
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-07-28 13:04:10 +0900

    solve: 2529 부등호 순열

--- kotlin/baekjoon/permutation/2529_부등호_순열.kt
+++ kotlin/baekjoon/permutation/2529_부등호_순열.kt
@@ -1,7 +1,32 @@
 package permutation
 
 fun main() {
+    val k = readln().toInt()
+    val operate = readln().split(" ")
 
+    val small = MutableList(k + 1) { it }
+    val big = MutableList(k + 1) { 9 - it }
+
+    while (true) {
+        if (check(small, operate)) break
+        if (!nextPermutation(small)) break
+    }
+    while (true) {
+        if (check(big, operate)) break
+        if (!prevPermutation(big)) break
+    }
+
+    println(big.joinToString(""))
+    println(small.joinToString(""))
+
+}
+
+fun check(nums: List<Int>, op: List<String>): Boolean {
+    for (i in 0 until op.size) {
+        if (op[i] == "<" && nums[i] > nums[i + 1]) return false
+        if (op[i] == ">" && nums[i] < nums[i + 1]) return false
+    }
+    return true
 }
 
 
@@ -22,3 +47,22 @@ fun nextPermutation(nums: MutableList<Int>): Boolean {
     }
     return true
 }
+
+fun prevPermutation(nums: MutableList<Int>): Boolean {
+    var i = nums.size - 1
+    while (i > 0 && nums[i - 1] <= nums[i]) i--
+    if (i <= 0) return false
+
+    var j = nums.size - 1
+    while (nums[j] >= nums[i - 1]) j--
+    nums[i - 1] = nums[j].also { nums[j] = nums[i - 1] }
+
+    j = nums.size - 1
+    while (i < j) {
+        nums[i] = nums[j].also { nums[j] = nums[i] }
+        i++
+        j--
+    }
+    return true
+}
+
