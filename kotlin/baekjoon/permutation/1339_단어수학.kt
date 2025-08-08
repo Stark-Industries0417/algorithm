56778a50426b1aa6fb8058a59262f8316a9c5045
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-08-08 14:56:33 +0900

    imporvement: 1339 단어수학 - 코드 개선

--- kotlin/baekjoon/permutation/1339_단어수학.kt
+++ kotlin/baekjoon/permutation/1339_단어수학.kt
@@ -1,3 +1,4 @@
+
 package permutation
 
 import kotlin.math.pow
@@ -6,18 +7,19 @@ import kotlin.math.pow
 fun main() {
     val n = readln().toInt()
     val words = List(n) { readln() }
-    val weights = mutableMapOf<Char, Int>()
-    words.forEach { word ->
-        word.forEachIndexed { index, c ->
-            val charWeights = 10.0.pow((word.length - index - 1).toDouble())
-            weights[c] = weights.getOrDefault(c, 0) + charWeights.toInt()
+    var ans = 0
+    words
+        .flatMap { word ->
+            word.mapIndexed { index, char ->
+                char to 10.0.pow(word.length - index - 1)
+            }
         }
-    }
-    var total = 0; var pow = 9
-    weights.values.sortedByDescending { it }
-        .forEach {
-             total += pow * it
-            pow -= 1
+        .groupBy { it.first }
+        .mapValues { (_, values) -> values.sumOf { it.second} }
+        .values
+        .sortedByDescending { it }
+        .forEachIndexed { index, weight ->
+            ans += ((9 - index) * weight).toInt()
         }
-    println(total)
+    println(ans)
 }
