61efed8408d3f8e67f5ed92a106a8609b98c7c8d
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-08-11 18:09:25 +0900

    solve: 14888 연산자 끼워넣기 - backtracking

--- kotlin/baekjoon/permutation/14888_연산자_끼워넣기.kt
+++ kotlin/baekjoon/permutation/14888_연산자_끼워넣기.kt
@@ -1,29 +1,29 @@
 package permutation
 
+
 fun main() {
     val n = readln().toInt()
     val nums = readln().split(" ").map { it.toInt() }
     val operNums = readln().split(" ").map { it.toInt() }
-    val operators = listOf('+', '-', '*', '/')
-    val operatorsList = mutableListOf<Char>()
-    List(4) {
-        repeat(operNums[it]) { _ ->
-            operatorsList.add(operators[it])
+
+    var maxValue = Int.MIN_VALUE
+    var minValue = Int.MAX_VALUE
+
+    fun backtrack(idx: Int, current: Int, plus: Int, minus: Int, mult: Int, dive: Int) {
+        if (idx == n) {
+            maxValue = maxOf(maxValue,current)
+            minValue = minOf(minValue,current)
+            return
         }
+
+        if (plus > 0) backtrack(idx + 1, current + nums[idx], plus - 1, minus, mult, dive)
+        if (minus > 0) backtrack(idx + 1, current - nums[idx], plus, minus - 1, mult, dive)
+        if (mult > 0) backtrack(idx + 1, current * nums[idx], plus, minus, mult - 1, dive)
+        if (dive > 0) backtrack(idx + 1, current / nums[idx], plus, minus, mult, dive - 1)
     }
 
+    backtrack(1, nums[0], operNums[0], operNums[1], operNums[2], operNums[3])
 
-
-}
-
-fun calc(operators: List<Char>, nums: MutableList<Int>): Int {
-    for (i in 1 until nums.size) {
-        when (operators[i - 1]) {
-            '+' -> nums[i] = nums[i - 1] + nums[i]
-            '-' -> nums[i] = nums[i - 1] - nums[i]
-            '*' -> nums[i] = nums[i - 1] * nums[i]
-            '/' -> nums[i] = nums[i - 1] / nums[i]
-        }
-    }
-    return nums[nums.size - 1]
+    println(maxValue)
+    println(minValue)
 }
