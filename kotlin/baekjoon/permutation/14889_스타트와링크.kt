6cdfe8a279b04b2c06a121ba79109f077658ebb5
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-08-14 10:20:01 +0900

    solve: 14889 스타트와링크 - 조합

--- kotlin/baekjoon/permutation/14889_스타트와링크.kt
+++ kotlin/baekjoon/permutation/14889_스타트와링크.kt
@@ -1,10 +1,42 @@
 package permutation
 
+import kotlin.math.abs
+
 fun main() {
     val n = readln().toInt()
     val graph = List(n) {
         readln().split(" ").map { it.toInt() }
     }
+    var ans = Int.MAX_VALUE
 
-    // 백 트래킹 조합으로 풀이 시도
+    fun makeCombinations(start: Int, count: Int, n: Int, r: Int, team: MutableList<Int>) {
+        if (count == r) {
+
+            val startTeam = calculateTeamAbility(team, graph)
+            val linkTeam = calculateTeamAbility((0 until n).filter { it !in team }, graph)
+
+            ans = minOf(abs(startTeam - linkTeam), ans)
+            return
+        }
+
+        for (i in start until n) {
+            team.add(i)
+            makeCombinations(i + 1, count + 1, n, r, team)
+            team.removeLast()
+        }
+    }
+    makeCombinations(0, 0, n, n / 2, mutableListOf())
+
+    println(ans)
+}
+
+fun calculateTeamAbility(team: List<Int>, graph: List<List<Int>>): Int {
+    var total = 0
+    for (i in team.indices) {
+        for (j in team.indices) {
+            if (i == j) continue
+            total += graph[team[i]][team[j]]
+        }
+    }
+    return total
 }
