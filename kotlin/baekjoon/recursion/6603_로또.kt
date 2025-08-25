6c747d72ffe55adad983c385e73713e0b3427a2e
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-08-26 07:55:06 +0900

    improvement: 6603_로또

--- kotlin/baekjoon/recursion/6603_로또.kt
+++ kotlin/baekjoon/recursion/6603_로또.kt
@@ -7,23 +7,18 @@ fun main() {
         val s = input.subList(1, input.size)
         if (k == 0) break
 
-        s.printLottoNumbers(0, k,BooleanArray(s.size), 0,mutableListOf())
+        s.printLottoNumbers(0, k, 0, listOf())
         println()
     }
 }
 
-private fun List<Int>.printLottoNumbers(idx: Int, k: Int, check: BooleanArray, start: Int, temp: MutableList<Int>) {
+private fun List<Int>.printLottoNumbers(idx: Int, k: Int, start: Int, temp: List<Int>) {
     if (idx == 6) {
         println(temp.joinToString(" "))
         return
     }
 
     for (i in start until k) {
-        if (check[i]) continue
-        check[i] = true
-        temp.add(this[i])
-        printLottoNumbers(idx + 1, k, check, i + 1, temp)
-        check[i] = false
-        temp.removeLast()
+        printLottoNumbers(idx + 1, k, i + 1, temp + this[i])
     }
 }
