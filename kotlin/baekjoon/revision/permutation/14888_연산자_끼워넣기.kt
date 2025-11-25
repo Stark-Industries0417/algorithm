e028fc13ebc501e2c83adbded1cf7553d1e08ab0
kwon <anstjdrnjs3@naver.com>
2025-11-26 08:01:06 +0900

    pending - 14888 연산자 끼워넣기 복습

---
+++ kotlin/baekjoon/revision/permutation/14888_연산자_끼워넣기.kt
@@ -0,0 +1,9 @@
+package revision.permutation
+
+fun main() {
+    val n = readln().toInt()
+    val nums = readln().split(" ").map { it.toInt() }.toIntArray()
+    val ops = readln().split(" ").map { it.toInt() }.toIntArray()
+
+    fun go()
+}
